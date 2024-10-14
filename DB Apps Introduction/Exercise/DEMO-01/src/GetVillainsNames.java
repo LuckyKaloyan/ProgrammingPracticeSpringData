cca8363ff0eff65263bc9bd3328c3c6ca06c2e93
Калоян Димитров <136224811+LuckyKaloyan@users.noreply.github.com>
2024-10-14 10:00:15 +0300

    Update

--- DB Apps Introduction/Exercise/DEMO-01/src/GetVillainsNames.java
+++ DB Apps Introduction/Exercise/DEMO-01/src/GetVillainsNames.java
@@ -2,7 +2,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Scanner;
 
 public class GetVillainsNames {
     public static void main(String[] args) throws SQLException {
